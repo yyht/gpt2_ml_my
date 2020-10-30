cc8f282d2927f1b3ff60502b888cc863e630b65b
天逸 <albert.xht@alibaba-inc.com>
2020-10-30 10:27:31 +0800

    fix

---
+++ gpt2_ml_download.sh
@@ -0,0 +1,8 @@
+git clone -q https://github.com/imcaspar/gpt2-ml
+cd /content/gpt2-ml
+mkdir -p /content/gpt2-ml/models/mega
+
+perl 3rd/gdown.pl/gdown.pl https://drive.google.com/open?id=1mT_qCQg4AWnAXTwKfsyyRWCRpgPrBJS3 models/mega/model.ckpt-220000.data-00000-of-00001
+wget -q --show-progress https://github.com/imcaspar/gpt2-ml/releases/download/v1.0/model.ckpt-220000.index -P models/mega
+wget -q --show-progress https://github.com/imcaspar/gpt2-ml/releases/download/v1.0/model.ckpt-220000.meta -P models/mega
+echo 'Download finished.🍺'
