745e0462e8b8bb2a431c7c7e5f8d6dacd831dff0
天逸 <albert.xht@alibaba-inc.com>
2020-10-30 16:47:29 +0800

    fi

--- train/modeling.py
+++ train/modeling.py
@@ -779,7 +779,7 @@ def sample(news_config: GroverConfig, initial_context, eos_token, min_len, ignor
         def cond(ctx, cache, probs):
             # ctx = tf.Print(ctx,[tf.shape(ctx)])
             is_eos = tf.reduce_all(tf.reduce_any(tf.equal(ctx[:,-1:], eos_token), axis=1))
-            # is_len = tf.greater(get_shape_list(ctx)[1], min_len)
+            is_len = tf.greater(get_shape_list(probs)[1], get_shape_list(ctx)[1]+64)
             return tf.logical_not(is_eos)
             # return tf.logical_not(tf.logical_and(is_eos, is_len))
 
