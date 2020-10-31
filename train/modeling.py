c0f327cf466be06ee07ebc1fe49efc34ab10624b
天逸 <albert.xht@alibaba-inc.com>
2020-10-31 10:07:25 +0800

    fi

--- train/modeling.py
+++ train/modeling.py
@@ -327,8 +327,8 @@ def gumbel_sample(logits, num_samples):
     sample_shape = shape + [num_samples]
     uniform_noise = tf.random.uniform(sample_shape, minval=0, maxval=1)
     gumbel_noise = -tf.log(-tf.log(uniform_noise + 1e-9) + 1e-9)
-    return tf.argmax(tf.nn.softmax(logits + gumbel_noise), 1,
-                              output_type=tf.int32)
+    gumbel_prob = tf.nn.softmax(logits + gumbel_noise, axis=1)
+    return tf.argmax(gumbel_prob, 1, output_type=tf.int32)
 
 def _top_p_sample(logits, ignore_ids=None, num_samples=1, p=0.9):
     """
