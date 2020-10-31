792a033912b234597008f87636a23ccc69b7ba1f
天逸 <albert.xht@alibaba-inc.com>
2020-10-31 23:13:56 +0800

    fi

--- scripts/prepare_nce_negative_sample.py
+++ scripts/prepare_nce_negative_sample.py
@@ -209,9 +209,8 @@ def _cjk_punctuation():
     return u'\uff02\uff03\uff04\uff05\uff06\uff07\uff08\uff09\uff0a\uff0b\uff0c\uff0d\uff0f\uff1a\uff1b\uff1c\uff1d\uff1e\uff20\uff3b\uff3c\uff3d\uff3e\uff3f\uff40\uff5b\uff5c\uff5d\uff5e\uff5f\uff60\uff62\uff63\uff64\u3000\u3001\u3003\u3008\u3009\u300a\u300b\u300c\u300d\u300e\u300f\u3010\u3011\u3014\u3015\u3016\u3017\u3018\u3019\u301a\u301b\u301c\u301d\u301e\u301f\u3030\u303e\u303f\u2013\u2014\u2018\u2019\u201b\u201c\u201d\u201e\u201f\u2026\u2027\ufe4f\ufe51\ufe54\u00b7\uff01\uff1f\uff61\u3002'
 
 def postprocess(text):
-    text = re.sub(' +', ' ', text)
     text = re.sub('\' (re|m|s|t|ve|d|ll) ', '\'\\1 ', text)
-    punctuation = self._cjk_punctuation() + '+-/={(<['
+    punctuation = _cjk_punctuation() + '+-/={(<['
     punctuation_regex = '|'.join([re.escape(p) for p in punctuation])
     punctuation_regex = '(%s) ' % punctuation_regex
     text = re.sub(punctuation_regex, '\\1', text)
